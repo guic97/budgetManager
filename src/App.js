7b03e13b7d6115a9b48a834c841ff6b380e77df4
Atila Guilherme <atilacarrao@hotmail.com>
2022-03-03 15:46:18 -0300

    Restarting the react practice project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className='main'>
-      <p>Hey im learning React</p>
-      <p>Teste do github</p>
-    </div>
+    <>Recomeçando!</>
   );
 }
 
