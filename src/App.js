504d4ba61303828f2606faea36debc3802c79d6f
Atila Guilherme <atilacarrao@hotmail.com>
2022-03-10 20:44:40 -0300

    starting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
 function App() {
   return (
-    <>Recomeçando!</>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">NewProject</Link>
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Home/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/company" element={<Company/>}></Route>
+        <Route path="/newproject" element={<NewProject/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
